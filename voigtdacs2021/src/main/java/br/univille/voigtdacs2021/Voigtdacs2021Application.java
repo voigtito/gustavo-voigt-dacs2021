3f9369c24e70b3f863d64cf427331c414a778af3
voigtito <voigt.gustavoh@gmail.com>
2021-05-19 00:58:44 +0000

    First class

---
+++ voigtdacs2021/src/main/java/br/univille/voigtdacs2021/Voigtdacs2021Application.java
@@ -0,0 +1,13 @@
+package br.univille.voigtdacs2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Voigtdacs2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Voigtdacs2021Application.class, args);
+	}
+
+}
---
+++ voigtdacs2021/src/test/java/br/univille/voigtdacs2021/Voigtdacs2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.voigtdacs2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Voigtdacs2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
